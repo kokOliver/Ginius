a5e80b52955c1bcbc40b98590456e82dae1f27e0
camillajo <camilla.jorgensen@outlook.com>
2024-11-15 19:53:00 +0100

    using router

--- backend/gin-library/main.go
+++ backend/gin-library/main.go
@@ -1,55 +1,13 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
-	"github.com/gin-gonic/gin"
-
-	"ginius/gin-library/data-access/models"
-	"ginius/gin-library/data-access/data"
+	"ginius/gin-library/router"
 )
 
 func main() {
-	router:= gin.Default()
-	router.GET("/gins", getGins)
-	router.GET("/gins/:id", getGinByID)
-	router.POST("/gins", postGin)
-
-	router.Run("localhost:8080")
-}
-
-// getGins returns all gin entries
-func getGins(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, data.Gins)
-}
-
-// postGin adds a new gin entry from the JSON received in the request body
-func postGin(c *gin.Context) {
-	var newGin models.GinEntry
-
-	if err := c.BindJSON(&newGin); err != nil {
-		return
-	}
-
-	data.Gins = append(data.Gins, newGin)
-	c.IndentedJSON(http.StatusCreated, newGin)
-}
-
-// getGinByID returns a gin entry by its ID
-func getGinByID(c *gin.Context) {
-	id := c.Param("id")
-	ginID, err := strconv.Atoi(id)
+	r := router.SetupRouter()
+	err := r.Run("localhost:8080")
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid gin ID"})
 		return
 	}
-
-	for _, gin := range data.Gins {
-		if gin.GinID == ginID {
-			c.IndentedJSON(http.StatusOK, gin)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "gin not found"})
 }
