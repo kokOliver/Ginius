642301d5299be26e7b27dd5af60cd02377e05c25
camillajo <camilla.jorgensen@outlook.com>
2024-11-15 19:48:35 +0100

    handlers moved

---
+++ backend/gin-library/handlers/gin_handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"ginius/gin-library/data"
+	"ginius/gin-library/models"
+	"github.com/gin-gonic/gin"
+)
+
+// GetGins returns all gin entries
+func GetGins(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, data.Gins)
+}
+
+// PostGin adds a new gin entry from the JSON received in the request body
+func PostGin(c *gin.Context) {
+	var newGin models.GinEntry
+
+	if err := c.BindJSON(&newGin); err != nil {
+		return
+	}
+
+	data.Gins = append(data.Gins, newGin)
+	c.IndentedJSON(http.StatusCreated, newGin)
+}
+
+// GetGinByID returns a gin entry by its ID
+func GetGinByID(c *gin.Context) {
+	id := c.Param("id")
+	ginID, err := strconv.Atoi(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid gin ID"})
+		return
+	}
+
+	for _, gin := range data.Gins {
+		if gin.GinID == ginID {
+			c.IndentedJSON(http.StatusOK, gin)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "gin not found"})
+}
